bd5bade148a12b0c9585077e050d7087fabcbf82
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2022-01-04 15:59:41 +0100

    Inizialize project

---
+++ src/main/java/it/unimib/unimibmodules/UnimibModulesApplication.java
@@ -0,0 +1,13 @@
+package it.unimib.unimibmodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnimibModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnimibModulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unimib/unimibmodules/UnimibModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unimib.unimibmodules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnimibModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
