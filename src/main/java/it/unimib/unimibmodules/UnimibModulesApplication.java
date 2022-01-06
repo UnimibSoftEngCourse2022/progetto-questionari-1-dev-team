857d88206037fddbff18b185713e9a542d644031
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-06 23:53:00 +0100

    Added ModelMapper dependency and bean

--- src/main/java/it/unimib/unimibmodules/UnimibModulesApplication.java
+++ src/main/java/it/unimib/unimibmodules/UnimibModulesApplication.java
@@ -1,13 +1,21 @@
 package it.unimib.unimibmodules;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UnimibModulesApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UnimibModulesApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+
+		return new ModelMapper();
+	}
 }
