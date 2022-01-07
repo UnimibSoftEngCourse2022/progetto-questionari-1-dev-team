8082ccf37f854dd26bdda438d59cb38780b2b196
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-07 17:43:25 +0100

    Added UserFactory class

---
+++ src/main/java/it/unimib/unimibmodules/factory/UserFactory.java
@@ -0,0 +1,35 @@
+package it.unimib.unimibmodules.factory;
+
+import it.unimib.unimibmodules.model.User;
+
+/**
+ * Factory class for User.
+ * @author Gianlorenzo Martini
+ */
+
+public class UserFactory {
+
+    /**
+	 * Creates a new instance of User.
+	 * @param   id          the id of the user
+	 * @param   email       the email chose by the user
+     * @param   password    the password chose by the user
+     * @param   username    the username of the user
+     * @param   name        the name of the user
+     * @param   surname     the surname of the user
+	 * @return              the new instance of User
+	 */
+    
+    public static User createUser(int id, String email, String password, String username, String name, String surname) {
+        
+        User user = new User();
+        user.setId(id);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setUsername(username);
+        user.setName(name);
+        user.setSurname(surname);
+
+        return user;
+    }
+}
