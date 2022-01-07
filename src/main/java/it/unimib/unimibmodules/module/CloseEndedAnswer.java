fa47e964a8ecf78569ce1af88a5e2a170620eb72
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-07 14:27:38 +0100

    Added CloseEndedAnswer

---
+++ src/main/java/it/unimib/unimibmodules/module/CloseEndedAnswer.java
@@ -0,0 +1,35 @@
+package it.unimib.unimibmodules.module;
+
+/**
+ * Represents a closed-ended answer.
+ * @author Davide Costantini
+ */
+public class CloseEndedAnswer extends Answer {
+
+	/**
+	 * Represents whether the user selected or not this closed-ended answer.
+	 */
+	private boolean chosen;
+
+	public CloseEndedAnswer() {
+
+	}
+
+	/**
+	 * Returns the value of chosen.
+	 * @return  true if this answer has been chosen by the user, false otherwise
+	 */
+	public boolean isChosen() {
+
+		return chosen;
+	}
+
+	/**
+	 * Modifies the value of chosen, setting <code>chosen</code> as the new value.
+	 * @param   chosen  the new chosen value
+	 */
+	public void setChosen(boolean chosen) {
+
+		this.chosen = chosen;
+	}
+}
