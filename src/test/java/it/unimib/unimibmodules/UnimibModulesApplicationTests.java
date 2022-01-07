755eb0e9532d247424ee0c4229625bf980e80ca9
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-07 14:23:48 +0100

    Added AnswerTest

--- src/test/java/it/unimib/unimibmodules/UnimibModulesApplicationTests.java
+++ src/test/java/it/unimib/unimibmodules/UnimibModulesApplicationTests.java
@@ -1,13 +1,8 @@
 package it.unimib.unimibmodules;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UnimibModulesApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
