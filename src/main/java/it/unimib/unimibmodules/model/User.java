fd06de0a65180512215127384a77755199b0dba2
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-07 17:42:33 +0100

    Added User class

---
+++ src/main/java/it/unimib/unimibmodules/model/User.java
@@ -0,0 +1,68 @@
+package it.unimib.unimibmodules.model;
+
+/**
+ * Rapresentation of a user in a general website way. 
+ * @author Gianlorenzo Martini
+ */
+
+public class User {
+    
+    private int id;
+    private String email;
+    private String password;
+    private String username;
+    private String name;
+    private String surname;
+
+    public User() {
+        
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+}
