cf452902ff2ac496d9682e2941a627972e77bb24
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2022-01-09 18:58:06 +0100

    Modified QuestionFactory

--- src/main/java/it/unimib/unimibmodules/factory/QuestionFactory.java
+++ src/main/java/it/unimib/unimibmodules/factory/QuestionFactory.java
@@ -17,20 +17,16 @@ public class QuestionFactory {
 	/**
 	 * Creates a new instance of Question without image.
 	 * @param   text    	the text of the question
-	 * @param   Answer    	the list of answers of the question
 	 * @param 	Category	the category of the question
 	 * @param  	User		the user who created the question
-	 * @param 	Survey		the list of surveys where the question is in
 	 * @return          the newly created instance of Question
 	 */
-	public static Question createQuestion(String text, Set<Answer> answer, Category category, User user, Set<Survey> survey) {
+	public static Question createQuestion(String text, Category category, User user) {
 		
 		Question question = new Question();
-		question.setAnswer(answer);
 		question.setText(text);
 		question.setCategory(category);
 		question.setUser(user);
-		question.setSurvey(survey);
 		
 		return question;
 	}
@@ -39,14 +35,13 @@ public class QuestionFactory {
 	/**
 	 * Creates a new instance of Question with image.
 	 * @param   text    	the text of the question
-	 * @param   Answer    	the list of answers of the question
 	 * @param 	Category	the category of the question
 	 * @param  	User		the user who created the question
 	 * @param 	urlImage	the image's url of the question
 	 * @param 	
 	 * @return          the newly created instance of Question
 	 */
-	public static Question createQuestionWithImage(String text, Set<Answer> answer, Category category, User user, Set<Survey> survey, String urlImage) {
+	public static Question createQuestionWithImage(String text, Category category, User user, String urlImage) {
 		
 		Question question = createQuestion(text, answer, category, user);
 		question.setUrlImage(urlImage);
