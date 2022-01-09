82b0bf3ece75602bc121b9e817763bb5eb17cc16
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-09 16:53:38 +0100

    Added setters to UserDTO

--- src/main/java/it/unimib/unimibmodules/dto/UserDTO.java
+++ src/main/java/it/unimib/unimibmodules/dto/UserDTO.java
@@ -63,31 +63,63 @@ public class UserDTO {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
     public String getUsername() {
         return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getSurname() {
         return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public List<QuestionDTO> getQuestionsCreated() {
         return questionsCreated;
     }
 
+    public void setQuestionsCreated(List<QuestionDTO> questionsCreated) {
+        this.questionsCreated = questionsCreated; 
+    }
+
     public List<SurveyDTO> getSurveysCreated() {
         return surveysCreated;
     }
 
+    public void setSurveysCreated(List<SurveyDTO> surveysCreated) {
+        this.surveysCreated = surveysCreated;
+    }
+
     public List<SurveyDTO> getSurveysReplied() {
         return surveysReplied;
+    }
+
+    public void setSurveysReplied(List<SurveyDTO> surveysReplied) {
+        this.surveysReplied = surveysReplied;
     }
 }
