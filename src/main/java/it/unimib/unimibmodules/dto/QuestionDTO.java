8cedfa245a849e2c127c867011c6ba251aa6434b
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2022-01-09 18:20:43 +0100

    Modified QuestionDTO

--- src/main/java/it/unimib/unimibmodules/dto/QuestionDTO.java
+++ src/main/java/it/unimib/unimibmodules/dto/QuestionDTO.java
@@ -1,5 +1,9 @@
 package it.unimib.unimibmodules.dto;
 
+import java.util.Set;
+
+import it.unimib.unimibmodules.model.Answer;
+
 /**
  * DTO for Question.
  * @author Khalil
@@ -11,24 +15,31 @@ public class QuestionDTO{
 	/**
 	 * Serialization of the category of the question.
 	 */
-	private String category;
+	private CategoryDTO categoryDTO;
 	
 	/**
 	 * Serialization of the image's url of the question.
 	 */
 	private String urlImage;
 	
-	
 	/**
 	 * Serialization of the text of the question.
 	 */
 	private String text;
 	
-	//rispostaDTO
+	/**
+	 * Serialization of the list of the answers.
+	 */
+	private Set<AnswerDTO> answerDTO;
 	
 	/**
 	 * Serialization of the id of the user who created the question.
 	 */
 	private UserDTO userDTO;
+	
+	/**
+	 * Serialization of the list of the questions.
+	 */
+	private Set<SurveyDTO> surveyDTO;
 	
 }
