4d0d79fd6047b6cf2171408dfc818a5213f64c60
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-12 20:39:33 +0100

    Added id and some DTOs to AnswerDTO and QuestionDTO

--- src/main/java/it/unimib/unimibmodules/dto/AnswerDTO.java
+++ src/main/java/it/unimib/unimibmodules/dto/AnswerDTO.java
@@ -1,5 +1,7 @@
 package it.unimib.unimibmodules.dto;
 
+import java.util.Set;
+
 /**
  * DTO for Answer and CloseEndedAnswer.
  * @author Davide Costantini
@@ -8,19 +10,52 @@ package it.unimib.unimibmodules.dto;
 public class AnswerDTO {
 
 	/**
+	 * Serialization of the id of the close-ended answer.
+	 */
+	private int id;
+
+	/**
 	 * Serialization of the text of the answer.
 	 */
 	private String text;
 
 	/**
-	 * Serialization of the chosen value of the answer.
-	 */
-	private boolean chosen;
-
-	/**
 	 * Serialization of the id of the user who created the answer.
 	 */
 	private UserDTO userDTO;
+
+	/**
+	 * Serialization of the survey to which this answer belongs.
+	 */
+	private SurveyDTO surveyDTO;
+
+	/**
+	 * Serialization of the question to which this answer belongs.
+	 */
+	private QuestionDTO questionDTO;
+
+	/**
+	 * Serialization of the list of close-ended answers related to this answer.
+	 */
+	private Set<CloseEndedAnswerDTO> closeEndedAnswerText;
+
+	/**
+	 * Returns the id of the answer.
+	 * @return	the id of the answer
+	 */
+	public int getId() {
+
+		return id;
+	}
+
+	/**
+	 * Modifies the id of the answer, setting <code>id</code> as the new value.
+	 * @param	id	the new id value
+	 */
+	public void setId(int id) {
+
+		this.id = id;
+	}
 
 	/**
 	 * Returns the text of the answer.
@@ -41,24 +76,6 @@ public class AnswerDTO {
 	}
 
 	/**
-	 * Returns the value of chosen.
-	 * @return  true if this answer has been chosen by the user, false otherwise
-	 */
-	public boolean isChosen() {
-
-		return chosen;
-	}
-
-	/**
-	 * Modifies the value of chosen, setting <code>chosen</code> as the new value.
-	 * @param chosen	the new chosen value
-	 */
-	public void setChosen(boolean chosen) {
-
-		this.chosen = chosen;
-	}
-
-	/**
 	 * Returns the DTO of the user who created the answer.
 	 * @return	an instance of UserDTO containing the user who created the answer
 	 */
@@ -74,5 +91,61 @@ public class AnswerDTO {
 	public void setUserDTO(UserDTO userDTO) {
 
 		this.userDTO = userDTO;
+	}
+
+	/**
+	 * Returns the DTO of the survey to which this answer belongs.
+	 * @return	an instance of SurveyDTO containing the survey to which this answer belongs
+	 */
+	public SurveyDTO getSurveyDTO() {
+
+		return surveyDTO;
+	}
+
+	/**
+	 * Modifies the DTO ofthe survey to which this answer belongs, setting <code>surveyDTO</code> as the new SurveyDTO.
+	 * @param	surveyDTO	the new UserDTO
+	 */
+	public void setSurveyDTO(SurveyDTO surveyDTO) {
+
+		this.surveyDTO = surveyDTO;
+	}
+
+
+	/**
+	 * Returns the DTO of the question to which this answer belongs.
+	 * @return	an instance of QuestionDTO containing the question to which this answer belongs
+	 */
+	public QuestionDTO getQuestionDTO() {
+
+		return questionDTO;
+	}
+
+	/**
+	 * Modifies the DTO of the question to which this answer belongs, setting <code>questionDTO</code> as the new QuestionDTO.
+	 * @param	questionDTO	the new QuestionDTO
+	 */
+	public void setQuestionDTO(QuestionDTO questionDTO) {
+
+		this.questionDTO = questionDTO;
+	}
+
+
+	/**
+	 * Returns the DTO of the list of close-ended answers related to this answer.
+	 * @return	an instance of CloseEndedAnswerDTO containing the list of close-ended answers related to this answer
+	 */
+	public Set<CloseEndedAnswerDTO> getCloseEndedAnswerText() {
+
+		return closeEndedAnswerText;
+	}
+
+	/**
+	 * Modifies the DTO of the list of close-ended answers related to this answer, setting <code>closeEndedAnswerText</code> as the new CloseEndedAnswerDTO.
+	 * @param	closeEndedAnswerText	the new CloseEndedAnswerDTO
+	 */
+	public void setCloseEndedAnswerText(Set<CloseEndedAnswerDTO> closeEndedAnswerText) {
+
+		this.closeEndedAnswerText = closeEndedAnswerText;
 	}
 }
