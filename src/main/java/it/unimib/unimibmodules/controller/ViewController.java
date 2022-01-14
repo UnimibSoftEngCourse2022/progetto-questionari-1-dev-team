ce22804b1d739103a1abf26cf32e549cd1a3bfb6
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-14 18:00:07 +0100

    Added ViewController

---
+++ src/main/java/it/unimib/unimibmodules/controller/ViewController.java
@@ -0,0 +1,20 @@
+package it.unimib.unimibmodules.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller handling views.
+ * @author Davide Costantini
+ * @version 0.1.0
+ */
+@Controller
+public class ViewController {
+
+	@GetMapping(path = "/")
+	public ModelAndView index() {
+
+		return new ModelAndView("index.html");
+	}
+}
