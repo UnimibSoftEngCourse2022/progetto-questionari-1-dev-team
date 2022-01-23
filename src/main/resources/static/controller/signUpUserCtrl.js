4d3415f1ef9a7ec1af3ab644e6f641a8580e81fc
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-23 01:10:59 +0100

    Added signup page and signUpUser controller.

---
+++ src/main/resources/static/controller/signUpUserCtrl.js
@@ -0,0 +1,31 @@
+app.controller('signUpUserCtrl', function($scope, $http) {
+
+    $scope.username = "";
+    $scope.name = "";
+    $scope.surname = "";
+    $scope.email = "";
+    $scope.password = "";
+
+    $scope.signUpUser = function() {
+
+        let data = {
+
+            username : $scope.username,
+            name : $scope.name,
+            surname : $scope.surname,
+            email : $scope.email,
+            password : $scope.password
+
+        }
+
+        $http.post("/api/signUpUser", data).then(function onFulfilled(response) {
+
+            console.log(response);
+
+        }, function errorCallback(response) {
+
+            console.error(response);
+        });
+    }
+});
+
