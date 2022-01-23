b7050977eb6fa2666430e5c5028695fec172fa4a
Lorenzo <l.occhipinti1@campus.unimib.it>
2022-01-23 12:29:21 +0100

    Fixed error in version_bump.ps1

--- version_bump.ps1
+++ version_bump.ps1
@@ -12,7 +12,7 @@ if ($null -eq $NEW_VERSION) {
 }
 
 Write-Output "Bumping version in $POM_LOCATION"
-(get-content $POM_LOCATION) -replace "<version>[0-9]\.[0-9]\.[0-9]-SNAPSHOT<\/version>","<version>$NEW_VERSION</version>" | Out-File -encoding ASCII $POM_LOCATION
+(get-content $POM_LOCATION) -replace "<version>[0-9]\.[0-9]\.[0-9]-SNAPSHOT<\/version>","<version>$NEW_VERSION-SNAPSHOT</version>" | Out-File -encoding ASCII $POM_LOCATION
 Write-Output "Bumping version in $PACKAGE_JSON_LOCATION"
 (get-content $PACKAGE_JSON_LOCATION) -replace "`"version`":\s`"[0-9]\.[0-9]\.[0-9]`"","`"version`": `"$NEW_VERSION`"" | Out-File -encoding ASCII $PACKAGE_JSON_LOCATION
 Get-ChildItem -Path src -Filter *.java -Recurse | ForEach-Object {
