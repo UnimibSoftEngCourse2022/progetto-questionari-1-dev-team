39fa51457405e55be2f1e2c4787665bce440d8c0
Lorenzo <l.occhipinti1@campus.unimib.it>
2022-01-25 21:33:28 +0100

    Corrected code smells in Category

--- src/main/java/it/unimib/unimibmodules/exception/EmptyCategoryException.java
+++
@@ -1,9 +0,0 @@
-package it.unimib.unimibmodules.exception;
-
-/**
- * Exception when a category doesn't have a name.
- * @author Lorenzo Occhipinti
- * @version 0.1.0
- */
-public class EmptyCategoryException extends Throwable{
-}
