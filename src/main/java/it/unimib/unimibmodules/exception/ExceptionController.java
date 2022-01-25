df19901d3492de9c9c70721a0ab6910c1b7cb288
Davide Costantini <d.costantini6@campus.unimib.it>
2022-01-26 00:29:55 +0100

    Fixed code smell "Printf-style format strings should be used correctly"

--- src/main/java/it/unimib/unimibmodules/exception/ExceptionController.java
+++ src/main/java/it/unimib/unimibmodules/exception/ExceptionController.java
@@ -37,7 +37,7 @@ public class ExceptionController {
     @ExceptionHandler(value = { NotFoundException.class })
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
 
-        LOGGER.error("Not found: " + ex.getMessage());
+        LOGGER.error("Not found: {}", ex.getMessage());
         return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
     }
 
