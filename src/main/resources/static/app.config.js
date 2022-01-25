576abe629936ab9a63bd24b0e8073ee9737aa0fe
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-25 02:00:05 +0100

    Fixed config and module angular files.

--- src/main/resources/static/app.config.js
+++ src/main/resources/static/app.config.js
@@ -7,27 +7,14 @@ angular.
 			$routeProvider.
 				when('/findSurvey', {
 					template: '<find-survey></find-survey>',
-				}).
-				when('/modifySurvey/:idSurvey/:idUser', {
+				}).when('/modifySurvey/:idSurvey/:idUser', {
 					template: '<modify-survey></modify-survey>'
-				}).
-				when('/editQuestion/:questionId/:userId', {
-					template: '<editQuestion></editQuestion>'
-				}).
-				when('/addSurvey/:idUser', {
-					template: '<addSurvey></addSurvey>'
-				}).
-				when('/compileSurvey/:idSurvey/:idUser', {
-					template: '<compileSurvey></compileSurvey>'
-				}).when("/", {
-					templateUrl: "/template/home.html",
-					controller: "homeCtrl"
-				}).when("/add_answer", {
-					templateUrl: "/template/add_answer.html",
-					controller: "addAnswerCtrl"
-				}).when("/edit_answer", {
-					templateUrl: "/template/edit_answer.html",
-					controller: "editAnswerCtrl"
+				}).when('/addSurvey/:idUser', {
+					template: '<add-survey></add-survey>'
+				}).when('/compileSurvey/:idSurvey/:idUser', {
+					template: '<compile-survey></compile-survey>'
+				}).when("/loginUser", {
+					template: '<login-user></login-user>'
 				}).otherwise({ redirectTo: "/findSurvey" });
 		}
 	]);
