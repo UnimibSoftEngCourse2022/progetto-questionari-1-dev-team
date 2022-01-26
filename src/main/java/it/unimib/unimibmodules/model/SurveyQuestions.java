aeced50a9769ccdae94c4563d70d65e3b6fab6a1
Khalil Mohamed Khalil <k.mohamedkhalil@campus.unimib.it>
2022-01-26 12:00:46 +0100

    Added javaDoc in SurveyQuestions and SurveyQuestionsDTO

--- src/main/java/it/unimib/unimibmodules/model/SurveyQuestions.java
+++ src/main/java/it/unimib/unimibmodules/model/SurveyQuestions.java
@@ -2,6 +2,12 @@ package it.unimib.unimibmodules.model;
 
 import javax.persistence.*;
 
+/**
+ * Represents a surveyQuestion relationship.
+ *
+ * @author Khalil Mohamed Khalil
+ * @version 0.2.0
+ */
 @Entity
 @Table(name = "surveyquestions")
 public class SurveyQuestions {
@@ -12,26 +18,48 @@ public class SurveyQuestions {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
+    /**
+     * The question of the relationship.
+     */
     @ManyToOne
     @JoinColumn(name = "question_id", nullable = false)
     private Question question;
 
+    /**
+     * The survey of the relationship.
+     */
     @ManyToOne
     @JoinColumn(name = "survey_id", nullable = false)
     private Survey survey;
 
+    /**
+     * Returns the question of the relationship.
+     * @return	the question of the relationship.
+     */
     public Question getQuestion() {
         return question;
     }
 
+    /**
+     * Modifies the question of the relationship, setting <code>question</code> as the new value.
+     * @param	question	the new question
+     */
     public void setQuestion(Question question) {
         this.question = question;
     }
 
+    /**
+     * Returns the survey of the relationship.
+     * @return	the survey of the relationship.
+     */
     public Survey getSurvey() {
         return survey;
     }
 
+    /**
+     * Modifies the survey of the relationship, setting <code>survey</code> as the new value.
+     * @param	survey	the new survey
+     */
     public void setSurvey(Survey survey) {
         this.survey = survey;
     }
