dccf2f538410e7e00b3a6006afe008527d23f25a
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-26 12:54:47 +0100

    Removed cookiesController.js.

--- src/main/resources/static/cookiesController.js
+++
@@ -1,8 +0,0 @@
-app.controller('cookiesController', function ($scope, $cookies) {
-
-    $scope.myCookieVal = $cookies.get('cookie');
-    $scope.setCookie = function (val) {
-        $cookies.put('cookie', val);
-    }
-
-});
