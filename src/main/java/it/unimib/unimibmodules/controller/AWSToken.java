5910de5b6f01d28648b78ebe45515a7e174e1e48
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-28 15:44:06 +0100

    Applied inversion of dependency to AWSToken.

--- src/main/java/it/unimib/unimibmodules/controller/AWSToken.java
+++ src/main/java/it/unimib/unimibmodules/controller/AWSToken.java
@@ -1,53 +1,20 @@
 package it.unimib.unimibmodules.controller;
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.cognitoidentity.AmazonCognitoIdentity;
-import com.amazonaws.services.cognitoidentity.AmazonCognitoIdentityClient;
-import com.amazonaws.services.cognitoidentity.model.GetOpenIdTokenForDeveloperIdentityRequest;
 import com.amazonaws.services.cognitoidentity.model.GetOpenIdTokenForDeveloperIdentityResult;
 
-import it.unimib.unimibmodules.exception.NotFoundException;
-
-import java.util.HashMap;
-
-import org.springframework.stereotype.Component;
-
 /**
- * Service class used to get an User's token from AWS Cognito.
+ * Service class used to get a User's token from AWS Cognito.
  * @author Khalil Mohamed
  * @author Luca Milazzo
  * @version 0.3.0
  */
-@Component("awsToken")
-public class AWSToken {
-    public static final Region REGION = Region.getRegion(Regions.EU_CENTRAL_1);
-    public static final String ACCESS_KEY_ID = ACCESS_KEY_ID_COGNITO;
-    public static final String ACCESS_KEY_VALUE = SECRET_ACESS_KEY_COGNITO;
-    public static final String IDENTITY_POOL_ID = "eu-central-1:581b95ad-2144-4e38-b112-028abe2bac0a";
-    public static final String LOGIN_PROVIDER = "login.progettoquestionari.dev";
-    public static final String BUCKET_NAME = "questionari-images";
+public interface AWSToken {
+
     /**
      * Get the User's token from AWS Cognito
      * @param	idUser the id of the logged user.
      * @return				GetOpenIdTokenForDeveloperIdentityResult instance
      */
-    public GetOpenIdTokenForDeveloperIdentityResult getToken(int idUser){
-        AmazonCognitoIdentity identityClient = new AmazonCognitoIdentityClient(
-                new BasicAWSCredentials(ACCESS_KEY_ID, ACCESS_KEY_VALUE)
-        );
-        identityClient.setRegion(REGION);
-        GetOpenIdTokenForDeveloperIdentityRequest request =
-                new GetOpenIdTokenForDeveloperIdentityRequest();
-        request.setIdentityPoolId(IDENTITY_POOL_ID);
-        HashMap<String,String> logins = new HashMap<>();
-        logins.put(LOGIN_PROVIDER, ""+idUser);
-        request.setLogins(logins);
-        request.setTokenDuration(60 * 5L);
-        GetOpenIdTokenForDeveloperIdentityResult response =
-                identityClient.getOpenIdTokenForDeveloperIdentity(request);
-        return response;
-    }
+    GetOpenIdTokenForDeveloperIdentityResult getToken(int idUser);
 
 }
--- src/main/java/it/unimib/unimibmodules/controller/QuestionController.java
+++ src/main/java/it/unimib/unimibmodules/controller/QuestionController.java
@@ -4,6 +4,7 @@ import it.unimib.unimibmodules.dto.QuestionDTO;
 import it.unimib.unimibmodules.exception.NotFoundException;
 import it.unimib.unimibmodules.model.Question;
 import it.unimib.unimibmodules.model.User;
+import it.unimib.unimibmodules.service.AWSTokenImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
@@ -172,9 +173,9 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		logger.debug("Get token for id +" + id + ".");
 		return new ResponseEntity<>("{\"token\":\""+response.getToken()+"\"," +
 				"\"identityToken\":\"" + response.getIdentityId() +"\" , " +
-				"\"region\":\""+ AWSToken.REGION+"\", " +
-				"\"identityPoolId\":\""+ AWSToken.IDENTITY_POOL_ID+"\", " +
-				"\"bucketName\":\""+ AWSToken.BUCKET_NAME+"\"}", HttpStatus.CREATED);
+				"\"region\":\""+ AWSTokenImpl.REGION+"\", " +
+				"\"identityPoolId\":\""+ AWSTokenImpl.IDENTITY_POOL_ID+"\", " +
+				"\"bucketName\":\""+ AWSTokenImpl.BUCKET_NAME+"\"}", HttpStatus.CREATED);
 	}
 
 	/**
@@ -199,9 +200,9 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		return new ResponseEntity<>("{\"idQuestion\":\""+ question.getId() +"\"," +
 				"\"token\":\""+response.getToken()+"\"," +
 				"\"identityToken\":\"" + response.getIdentityId() +"\" , " + 
-				"\"region\":\""+ AWSToken.REGION+"\", " +
-				"\"identityPoolId\":\""+ AWSToken.IDENTITY_POOL_ID+"\", " +
-				"\"bucketName\":\""+ AWSToken.BUCKET_NAME+"\"}", HttpStatus.CREATED);
+				"\"region\":\""+ AWSTokenImpl.REGION+"\", " +
+				"\"identityPoolId\":\""+ AWSTokenImpl.IDENTITY_POOL_ID+"\", " +
+				"\"bucketName\":\""+ AWSTokenImpl.BUCKET_NAME+"\"}", HttpStatus.CREATED);
 	}
 
 	/**
