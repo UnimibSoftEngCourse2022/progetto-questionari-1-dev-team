b46fdc2b1fa61b09ca0eb9f3656ae07f6f986290
Luca <l.milazzo1@campus.unimib.it>
2022-01-28 07:09:35 +0100

    Added functional AWS architecture for loading images

--- src/main/java/it/unimib/unimibmodules/controller/QuestionController.java
+++ src/main/java/it/unimib/unimibmodules/controller/QuestionController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.amazonaws.services.cognitoidentity.model.GetOpenIdTokenForDeveloperIdentityResult;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +27,8 @@ import java.util.List;
 @RequestMapping("/api")
 public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 
-    private static final Logger logger = LogManager.getLogger(QuestionController.class);
-	
+	private static final Logger logger = LogManager.getLogger(QuestionController.class);
+
 	/**
 	 * Instance of QuestionRepository that will be used to access the db.
 	 */
@@ -42,14 +44,17 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 	 */
 	private final CategoryRepository categoryRepository;
 
+	private final AWSToken awsToken;
+
 	@Autowired
 	public QuestionController(QuestionRepository questionRepository, UserRepository userRepository, ModelMapper modelMapper,
-							  CategoryRepository categoryRepository) {
+							  CategoryRepository categoryRepository, AWSToken awsToken) {
 
 		super(modelMapper);
 		this.questionRepository = questionRepository;
 		this.userRepository = userRepository;
 		this.categoryRepository = categoryRepository;
+		this.awsToken = awsToken;
 
 		modelMapper.createTypeMap(Question.class, QuestionDTO.class)
 				.addMappings(mapper -> {
@@ -70,11 +75,11 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 					mapper.map(QuestionDTO::getQuestionType, Question::setQuestionType);
 				});
 
-        modelMapper.createTypeMap(User.class, QuestionDTO.class)
-                .addMapping(User::getId, (questionDTO, id) -> questionDTO.getUser().setId(id));
+		modelMapper.createTypeMap(User.class, QuestionDTO.class)
+				.addMapping(User::getId, (questionDTO, id) -> questionDTO.getUser().setId(id));
 
 	}
-	
+
 	/**
 	 * Gets the Question associated with the given id.
 	 * @param	id	the id of the question
@@ -84,7 +89,7 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 	@GetMapping(path = "/getQuestion/{id}")
 	public ResponseEntity<QuestionDTO> getQuestion(@PathVariable int id) throws NotFoundException{
 
-        Question question = questionRepository.get(id);
+		Question question = questionRepository.get(id);
 		logger.debug("Retrieved Question with id "+ id + ".");
 		return new ResponseEntity<>(convertToDTO(question), HttpStatus.OK);
 	}
@@ -99,8 +104,8 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		Iterable<Question> questionList = questionRepository.getAll();
 		List<QuestionDTO> questionDTOList = convertListToDTO(questionList);
 		logger.debug("Retrieved all the questions.");
-    return new ResponseEntity<>(questionDTOList, HttpStatus.OK);
-   }
+		return new ResponseEntity<>(questionDTOList, HttpStatus.OK);
+	}
 
 	/**
 	 * Gets all the questions of the user
@@ -128,7 +133,7 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		return new ResponseEntity<>(questionDTOList, HttpStatus.OK);
 	}
 
- 	/**
+	/**
 	 * Gets the question in the database where text is contained in the text of the question
 	 * @param	text	the text of the question to be found
 	 * @return		an HTTP response with status 200 and the QuestionDTO if the question has been found, 500 otherwise
@@ -160,7 +165,7 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		logger.debug("Retrieved {} questions for survey with id {}.", questionDTOList.size(), id);
 		return new ResponseEntity<>(questionDTOList, HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Creates a question, with the given text and id
 	 * @param	questionDTO the serialized object of the question
@@ -168,14 +173,26 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 	 * @throws	NotFoundException	if no user or category identified by <code>id</code> has been found
 	 */
 	@PostMapping(path = "/addQuestion")
-	public ResponseEntity<Integer> addQuestion(@RequestBody QuestionDTO questionDTO) throws NotFoundException {
+	public ResponseEntity<String> addQuestion(@RequestBody QuestionDTO questionDTO) throws NotFoundException {
 
 		Question question = convertToEntity(questionDTO);
 		questionRepository.add(question);
+
+		if(questionDTO.getUrlImage().equals("ok")){
+			question.setUrlImage(question.getId() + ".jpg");
+			questionRepository.modify(question);
+		}
+		
+		GetOpenIdTokenForDeveloperIdentityResult response = awsToken.getToken(question.getUser().getId());
 		logger.debug("Added Question with id +" + question.getId() + ".");
-		return new ResponseEntity<>(question.getId(), HttpStatus.CREATED);
+		return new ResponseEntity<>("{\"idQuestion\":\""+ question.getId() +"\"," +
+				"\"token\":\""+response.getToken()+"\"," +
+				"\"identityToken\":\"" + response.getIdentityId() +"\" , " + 
+				"\"region\":\""+ AWSToken.REGION+"\", " +
+				"\"identityPoolId\":\""+ AWSToken.IDENTITY_POOL_ID+"\", " +
+				"\"bucketName\":\""+ AWSToken.BUCKET_NAME+"\"}", HttpStatus.CREATED);
 	}
-	
+
 	/**
 	 * Modifies the question's text associated with the given id.
 	 * @param	questionDTO the serialized object of the question
@@ -190,7 +207,7 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		logger.debug("Modified Question with id " + question.getId() + ".");
 		return new ResponseEntity<>("Question modified.", HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Deletes the question associated with the given id.
 	 * @param   id	the id of the question that will be deleted
@@ -204,7 +221,7 @@ public class QuestionController extends DTOListMapping<Question, QuestionDTO>{
 		logger.debug("Removed Question with id " + id + ".");
 		return new ResponseEntity<>("Question deleted", HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Converts an instance of Question to an instance of questionDTO
 	 * @param   question	an instance of Question
--- src/main/resources/static/add-question/add-question.component.js
+++ src/main/resources/static/add-question/add-question.component.js
@@ -1,157 +1,211 @@
 'use strict';
 
 angular.
-module('UNIMIBModules').
-component('addQuestion', {
-	templateUrl: 'add-question/add-question.template.html',
-	controller: ['$location', '$routeParams', '$scope', '$http',
-		function addQuestionController($location, $routeParams, $scope, $http){
-			let tmpObj = {};
-			let tmpAnswer = {};
-			let user;
-			let qType;
-			let v;
-			let counter = 0;
-			let start = true;
-			let check;
-			$scope.imageView = false;
+	module('UNIMIBModules').
+	component('addQuestion', {
+		templateUrl: 'add-question/add-question.template.html',
+		controller: ['$location', '$routeParams', '$scope', '$http',
+			function addQuestionController($location, $routeParams, $scope, $http) {
+				let tmpObj = {};
+				let tmpAnswer = {};
+				let user;
+				let qType;
+				let v;
+				let counter = 0;
+				let start = true;
+				let check;
+				let photofile
+				$scope.imageView = false;
+				$scope.prova = ""
 
-			$scope.file_changed = function(element) {
+				$scope.file_changed = function(element) {
 
-				if(element === null){
-					$scope.imageView = false;
-				}else{
-					let photofile = element.files[0];
-
-					if(photofile === undefined)
+					if (element === null) {
 						$scope.imageView = false;
-					else{
-						let reader = new FileReader();
-						reader.onload = function(e) {
-							$scope.$apply(function() {
-								$scope.prev_img = e.target.result;
-								$scope.imageView = true;
-							});
-						};
-						reader.readAsDataURL(photofile);
-					}
-				}
+						photofile = undefined;
+					} else {
+						photofile = element.files[0];
 
-
-
-			};
-
-			$http.get("http://localhost:5000/api/findCategories")
-				.then(function(response) {
-					$scope.categories =  response.data;
-				});
-
-			$http.get("http://localhost:5000/api/getUser/1")
-				.then(function(response) {
-					user = response.data;
-				});
-
-			$scope.showQuestionType = function (value) {
-				v = value;
-
-				if(value === 0) {
-					$scope.errorAnswer = false;
-					counter = 0;
-					$scope.questionAnswer = [];
-					qType = "OPEN";
-					$scope.showHideClosedM = false;
-				}
-
-				if(value === 1) {
-					$scope.errorAnswer = false;
-					if(start)
-						$scope.questionAnswer = [];
-					start = false;
-					qType = "MULTIPLECLOSED";
-					$scope.showHideClosedM = true;
-				}
-
-				if(value === 2) {
-					$scope.errorAnswer = false;
-					if(start)
-						$scope.questionAnswer = [];
-					start = false;
-					qType = "SINGLECLOSED";
-					$scope.showHideClosedM = true;
-				}
-			};
-
-			$scope.newAnswer = function ($event) {
-				counter++;
-				$scope.questionAnswer.push({text : ''});
-				$event.preventDefault();
-			};
-
-			$scope.removeAnswer = function ($index) {
-				$scope.questionAnswer.splice($index, 1);
-				counter--;
-			};
-
-			$scope.addQuestion = function() {
-				$scope.errorQuestion = false;
-				$scope.errorCategory = false;
-				$scope.errorQuestionType = false;
-				$scope.errorAnswer = false;
-
-				check=true;
-
-				if($scope.questiontext == null || $scope.questiontext === "") {
-					$scope.errorQuestion = true;
-					check = false;
-				}
-
-				if($scope.categorySelected == null){
-					$scope.errorCategory = true;
-					check = false;
-				}
-
-				if(v == null){
-					$scope.errorQuestionType = true;
-					check = false;
-				}
-
-				if($scope.questionAnswer != null)
-					angular.forEach($scope.questionAnswer, function (question, key) {
-						if(counter < 2 || question.text == null || question.text === ""){
-							check = false;
-							$scope.errorAnswer = true;
+						if (photofile === undefined)
+							$scope.imageView = false;
+						else {
+							let reader = new FileReader();
+							reader.onload = function(e) {
+								$scope.$apply(function() {
+									$scope.prev_img = e.target.result;
+									$scope.imageView = true;
+								});
+							};
+							reader.readAsDataURL(photofile);
 						}
+					}
+				};
+
+				$http.get("/api/findCategories")
+					.then(function(response) {
+						$scope.categories = response.data;
 					});
 
-				if(v !== 0 && counter === 0){
-					check = false;
-					$scope.errorAnswer = true;
-				}
+				$http.get("/api/getUser/1")
+					.then(function(response) {
+						user = response.data;
+					});
 
-				if(check) {
-					tmpObj["user"] = angular.fromJson(user);
-					tmpObj["category"] = angular.fromJson($scope.categorySelected);
-					tmpObj["text"] = $scope.questiontext;
-					tmpObj["questionType"] = qType;
+				$scope.showQuestionType = function(value) {
+					v = value;
 
-					$http.post("http://localhost:5000/api/addQuestion", tmpObj, 'application/json; charset=utf-8')
-						.then(function (response) {
-							if(v!=0){
-								angular.forEach($scope.questionAnswer, function (question, key) {
-									question["questionDTO"] = {"id": response.data};
+					if (value === 0) {
+						$scope.errorAnswer = false;
+						counter = 0;
+						$scope.questionAnswer = [];
+						qType = "OPEN";
+						$scope.showHideClosedM = false;
+					}
 
-									$http.post("http://localhost:5000/api/addCloseEndedAnswer", question, 'application/json; charset=utf-8')
-										.then(function (response) {
-											$scope.prova = "Domanda inserita con successo!";
-										});
-								});
-							}else
-								$scope.prova = "Domanda inserita con successo!";
+					if (value === 1) {
+						$scope.errorAnswer = false;
+						if (start)
+							$scope.questionAnswer = [];
+						start = false;
+						qType = "MULTIPLECLOSED";
+						$scope.showHideClosedM = true;
+					}
 
+					if (value === 2) {
+						$scope.errorAnswer = false;
+						if (start)
+							$scope.questionAnswer = [];
+						start = false;
+						qType = "SINGLECLOSED";
+						$scope.showHideClosedM = true;
+					}
+				};
+
+				$scope.newAnswer = function($event) {
+					counter++;
+					$scope.questionAnswer.push({ text: '' });
+					$event.preventDefault();
+				};
+
+				$scope.removeAnswer = function($index) {
+					$scope.questionAnswer.splice($index, 1);
+					counter--;
+				};
+
+
+				$scope.addQuestion = function() {
+					$scope.errorQuestion = false;
+					$scope.errorCategory = false;
+					$scope.errorQuestionType = false;
+					$scope.errorAnswer = false;
+
+					check = true;
+
+					if ($scope.questiontext == null || $scope.questiontext === "") {
+						$scope.errorQuestion = true;
+						check = false;
+					}
+
+					if ($scope.categorySelected == null) {
+						$scope.errorCategory = true;
+						check = false;
+					}
+
+					if (v == null) {
+						$scope.errorQuestionType = true;
+						check = false;
+					}
+
+					if ($scope.questionAnswer != null)
+						angular.forEach($scope.questionAnswer, function(question, key) {
+							if (counter < 2 || question.text == null || question.text === "") {
+								check = false;
+								$scope.errorAnswer = true;
+							}
 						});
-				}
 
+					if (v !== 0 && counter === 0) {
+						check = false;
+						$scope.errorAnswer = true;
+					}
 
-			};
-	}]
-});
+					if (check) {
+						tmpObj["user"] = angular.fromJson(user);
+						tmpObj["category"] = angular.fromJson($scope.categorySelected);
+						tmpObj["text"] = $scope.questiontext;
+						tmpObj["questionType"] = qType;
+
+						if (photofile !== undefined)
+							tmpObj["urlImage"] = "ok";
+
+						$http.post("/api/addQuestion", tmpObj, 'application/json; charset=utf-8')
+							.then(function(response) {
+								if (v !== 0) {
+									angular.forEach($scope.questionAnswer, function(question, key) {
+										question["questionDTO"] = { "id": response.data };
+
+										$http.post("http://localhost:5000/api/addCloseEndedAnswer", question, 'application/json; charset=utf-8')
+											.then(function(response) {
+												$scope.prova = "Domanda inserita con successo!";
+											});
+									});
+
+								} else {
+									$scope.token = response.data.token
+									$scope.identityToken = response.data.identityToken
+									$scope.region = response.data.region
+									$scope.identityPoolId = response.data.identityPoolId
+									$scope.bucketName = response.data.bucketName
+									
+									if (photofile !== undefined) {
+										AWS.config.region = $scope.region;
+										AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+											IdentityId: $scope.identityToken,
+											IdentityPoolId: response.data.identityPoolId,
+											Logins: {
+												'cognito-identity.amazonaws.com': $scope.token,
+											}
+										});
+
+										AWS.config.credentials.get(function() {
+											var accessKeyId = AWS.config.credentials.accessKeyId;
+											var secretAccessKey = AWS.config.credentials.secretAccessKey;
+											var sessionToken = AWS.config.credentials.sessionToken;
+
+										});
+
+										var identityId = AWS.config.credentials.identityId;
+
+										let albumBucketName = $scope.bucketName;
+
+										let s3 = new AWS.S3({
+											params: { Bucket: albumBucketName }
+										});
+
+										let fileName = response.data.idQuestion + ".jpg";
+
+										let upload = new AWS.S3.ManagedUpload({
+											params: {
+												Bucket: albumBucketName,
+												Key: fileName,
+												Body: photofile
+											}
+										});
+
+										let promise = upload.promise();
+
+										promise.then(function(data) {
+												alert("OK")
+											},
+											function(err) {
+												alert(err.message)
+												$scope.prova = err.message;
+											}
+										);
+									}
+								}
+							});
+					}
+				};
+			}]
+	});
