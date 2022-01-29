d5062191c5e88b8a0789672f9ce6dd6ab06ed429
Gianlorenzo Martini <g.martini9@campus.unimib.it>
2022-01-29 01:57:28 +0100

    Removed old navbar controller.

--- src/main/resources/static/controller/navbarCtrl.js
+++
@@ -1,9 +0,0 @@
-app.controller('logoutUserCtrl', function ($scope) {
-
-    $scope.logoutUser = function () {
-        if (authService.isLoggedIn()) {
-            authService.setUser(undefined);
-            alert("You have just logged out!");
-        }
-    }
-});
